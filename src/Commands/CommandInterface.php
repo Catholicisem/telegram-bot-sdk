317be625e85a0d3aeb185be88998d52f5f09e736
Catholicisem <catholicisem@gmail.com>
2016-07-28 21:59:23 +0430

    Update CommandInterface.php

--- src/Commands/CommandInterface.php
+++ src/Commands/CommandInterface.php
@@ -1,51 +1,54 @@
 <?php
 
-namespace Telegram\Bot\Commands;
+namespace Vendor\App\Commands;
 
-use Telegram\Bot\Api;
-use Telegram\Bot\Objects\Update;
+use Telegram\Bot\Actions;
+use Telegram\Bot\Commands\Command;
 
-/**
- * Interface CommandInterface.
- */
-interface CommandInterface
+class StartCommand extends Command
 {
     /**
-     * Get Command Name.
-     *
-     * The name of the Telegram command.
-     * Ex: help - Whenever the user sends /help, this would be resolved.
-     *
-     * @return string
+     * @var string Command Name
      */
-    public function getName();
+    protected $name = "start";
 
     /**
-     * Get Command Aliases
-     *
-     * Helpful when you want to trigger command with more than one name.
-     *
-     * @return array
+     * @var string Command Description
      */
-    public function getAliases();
+    protected $description = "Start Command to get you started";
 
     /**
-     * Get Command Description.
-     *
-     * The Telegram command description.
-     *
-     * @return string
+     * @inheritdoc
      */
-    public function getDescription();
+    public function handle($arguments)
+    {
+        // This will send a message using `sendMessage` method behind the scenes to
+        // the user/chat id who triggered this command.
+        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
+        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
+        $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
 
-    /**
-     * Process Inbound Command.
-     *
-     * @param Api $telegram
-     * @param string $arguments
-     * @param Update $update
-     *
-     * @return mixed
-     */
-    public function make(Api $telegram, $arguments, Update $update);
+        // This will update the chat status to typing...
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
+
+        // This will prepare a list of available commands and send the user.
+        // First, Get an array of all registered commands
+        // They'll be in 'command-name' => 'Command Handler Class' format.
+        $commands = $this->getTelegram()->getCommands();
+
+        // Build the list
+        $response = '';
+        foreach ($commands as $name => $command) {
+            $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
+        }
+
+        // Reply with the commands list
+        $this->replyWithMessage(['text' => $response]);
+
+        // Trigger another command dynamically from within this command
+        // When you want to chain multiple commands within one or process the request further.
+        // The method supports second parameter arguments which you can optionally pass, By default
+        // it'll pass the same arguments that are received for this command originally.
+        $this->triggerCommand('subscribe');
+    }
 }
